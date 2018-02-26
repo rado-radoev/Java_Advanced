bbf0934f4e75a4b0e70a431b28c3229366a172e1
Radoslav Radoev <rradoev@gmail.com>
2018-02-26 15:30:33 -0800

    assignment 3 ready

--- _Advanced_Assignment3/src/com/knockknock/exceptions/ExceptionHandler.java
+++ _Advanced_Assignment3/src/com/knockknock/exceptions/ExceptionHandler.java
@@ -2,13 +2,30 @@ package com.knockknock.exceptions;
 
 import javax.swing.JOptionPane;
 
+/**
+* <h1>Exception Handler</h1> <br>* <strong>NOT USED</strong>
+* <p>Class that handles exceptions and displays in JOptionPane
+*
+* @author  Radoslav Radoev
+* @version %I%, %G%
+* @since   02/25/2018
+* 
+*/
 public class ExceptionHandler {
 	
+	/**
+	 * Method to handle excceptions
+	 * @param exp the exception to handle
+	 */
 	public static void handleException(Exception exp) {
 		displayExceptionMessage(exp);
 
 	}
 	
+	/**
+	 * Method to display exception message and cause in JOptionPane
+	 * @param exp the exception to handle
+	 */
 	private static void displayExceptionMessage(Exception exp) {
 		javax.swing.JOptionPane.showMessageDialog(null, 
 				exp.getMessage(), 
